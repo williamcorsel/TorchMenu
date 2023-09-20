ab10ae008638a8a4c2a912d4aab1bab9a2d1c058
William Corsel <c.w.corsel@umail.leidenuniv.nl>
2023-09-20 22:48:17 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -18,10 +18,17 @@ setuptools.setup(
     name='torchmenu',
     version=get_version('torchmenu/__version__.py'),
     description='TorchServe GUI',
+    long_description=open('README.md').read(),
     python_requires='>=3.10',
     packages=setuptools.find_packages(exclude=['test', 'docs']),
-    install_requires=get_requirements('requirements/requirements.txt'),
+    install_requires=get_requirements('requirements.txt'),
+    include_package_data=True,
     extras_require={
-        'dev': get_requirements('requirements/requirements-dev.txt'),
-    }
+        'dev': get_requirements('requirements-dev.txt'),
+    },
+    entry_points={
+        'console_scripts': [
+            'torchmenu = torchmenu.cli:main',
+        ],
+    },
 )
